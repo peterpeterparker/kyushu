754c002900e4cf14fcca29717f7db30cadc565c4
David Dal Busco <david.dalbusco@outlook.com>
2026-06-03 16:07:44 +0200

    feat(docs): static file server simplified example (#50)

--- packages/docs/astro.config.mjs
+++ packages/docs/astro.config.mjs
@@ -83,6 +83,7 @@ export default defineConfig({
             { slug: "examples/fetch" },
             { slug: "examples/file-system" },
             { slug: "examples/env" },
+            { slug: "examples/static-file-server" },
           ],
         },
       ],
