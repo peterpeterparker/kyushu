8cbe889ef8850744b43a85a6e3d90aa0ab6e2190
David Dal Busco <david.dalbusco@outlook.com>
2026-06-25 09:19:37 +0200

    feat: issue

--- examples/hello-world/src/index.ts
+++ examples/hello-world/src/index.ts
@@ -1,7 +1,11 @@
 import type { ExportedHandler } from "kyushu-types";
+import { writeFileSync, readFileSync } from "node:fs";
 
 export default {
   async fetch(request, env) {
+    writeFileSync("/hello.txt", "Hello from Kyushu!");
+    const content = readFileSync("/hello.txt", "utf8");
+
     console.log(request, env);
 
     return await env.ASSETS.fetch(request);
