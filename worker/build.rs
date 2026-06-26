1872c20d6c1884374a6f2848ce86059b3af4ff1c
David Dal Busco <david.dalbusco@outlook.com>
2026-06-26 07:13:22 +0200

    chore(worker): build worker lib with crate (#117)

--- worker/build.rs
+++ worker/build.rs
@@ -1,18 +1,21 @@
 use std::process::Command;
 
 fn main() {
-    let watch_path = "../packages/types/src";
-    println!("cargo:rerun-if-changed={watch_path}");
+    for package in &["kyushu-types", "kyushu-worker"] {
+        let watch_path = format!("../packages/{}/src", package.replace("kyushu-", ""));
+        println!("cargo:rerun-if-changed={watch_path}");
 
-    let dist = std::path::Path::new("../packages/types/dist");
-    if !dist.exists() {
-        println!("cargo:rerun-if-changed=");
+        let dist_path = format!("../packages/{}/dist", package.replace("kyushu-", ""));
+        let dist = std::path::Path::new(&dist_path);
+        if !dist.exists() {
+            println!("cargo:rerun-if-changed=");
+        }
+
+        let status = Command::new("pnpm")
+            .args(["--filter", package, "build"])
+            .status()
+            .expect(&format!("failed to run pnpm build for {package}"));
+
+        assert!(status.success(), "{package} build failed");
     }
-
-    let status = Command::new("pnpm")
-        .args(["--filter", "kyushu-types", "build"])
-        .status()
-        .expect("failed to run pnpm build");
-
-    assert!(status.success(), "kyushu-types build failed");
 }
