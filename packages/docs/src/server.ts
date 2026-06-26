581c698b94e10cf8a4ae71535c2efaf81d2d30c6
David Dal Busco <david.dalbusco@outlook.com>
2026-06-26 08:37:03 +0200

    feat(worker): consumer deal with custom headers

--- packages/docs/src/server.ts
+++ packages/docs/src/server.ts
@@ -1,7 +1,22 @@
 import type { ExportedHandler } from "kyushu-types";
 
+const CUSTOM_MIME_TYPES: Record<string, string> = {
+  "/install": "text/x-shellscript",
+};
+
 export default {
   async fetch(request, env) {
-    return await env.ASSETS.fetch(request);
+    const response = await env.ASSETS.fetch(request);
+
+    const { pathname } = URL.parse(request.url) ?? { pathname: null };
+    const customMimeType = pathname != null ? CUSTOM_MIME_TYPES[pathname] : undefined;
+
+    return {
+      ...response,
+      headers: {
+        ...response.headers,
+        "content-type": customMimeType ?? response.headers["content-type"],
+      },
+    };
   },
 } satisfies ExportedHandler;
